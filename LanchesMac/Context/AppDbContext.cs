eddeff2da8c1cb9a6ef55c83c93debd71d293028
Victor <vmendessforne@gmail.com>
2024-05-20 17:37:55 -0300

    Adicionar arquivos de projeto.

---
+++ LanchesMac/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }// o EFC sabe que a partir da classe Categoria ele precisa criar uma tabela chamada Categorias
+        public DbSet<Capinha> Capinhas { get; set; }
+    }
+}
