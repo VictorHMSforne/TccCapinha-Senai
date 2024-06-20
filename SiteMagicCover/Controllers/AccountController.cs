64d36befca4827c814d08706c9e0b80bf4187a84
Victor <vmendessforne@gmail.com>
2024-06-20 19:13:11 -0300

    modificações no admin

--- SiteMagicCover/Controllers/AccountController.cs
+++ SiteMagicCover/Controllers/AccountController.cs
@@ -88,5 +88,10 @@ namespace SiteMagicCover.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
     }
 }
